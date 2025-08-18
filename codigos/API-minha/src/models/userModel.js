13b659f2a873073eecb13a594dc59d46de6f3af2
leuthier <leuthier@users.noreply.github.com>
2025-08-18 02:11:17 -0300

    Two users saved on memory BD and fixed a bug - sender and receiver can not be same user

--- codigos/API-minha/src/models/userModel.js
+++ codigos/API-minha/src/models/userModel.js
@@ -1,5 +1,23 @@
 // Banco de dados em memória
-const users = [{"email":"victor@leuthier.com", "password":"1234"}];
+const users = [
+  {
+    "email": "victor@leuthier.com"
+  },
+  {
+    "id": "eb2d3111-4f9b-4bbe-88ec-89c623c49a46",
+    "name": "andre",
+    "email": "andre@gmail.com",
+    "favored": false,
+    "balance": 10
+  },
+  {
+    "id": "84e47cef-4783-4398-9d72-6eed9d67bf8e",
+    "name": "victor2",
+    "email": "victor@leuth.com",
+    "favored": true,
+    "balance": 100
+  }
+]
 
 module.exports = {
   getAll: () => users,
--- codigos/API-minha/src/services/transferService.js
+++ codigos/API-minha/src/services/transferService.js
@@ -8,25 +8,28 @@ function transfer({ fromId, toId, amount }){
 
   if(!from) throw {
     status: 404, message: 'Remetente não encontrado'
-};
+  };
 
   if(!to) throw {
     status: 404, message: 'Destinatário não encontrado'
-};
+  };
 
   if(amount <= 0) throw {
     status: 400, message: 'Valor deve ser maior que zero'
-};
+  };
 
+  if(from.id === to.id) throw {
+    status: 400, message: 'Não é possível transferir para si mesmo'
+  };
 
   // regra: transferências para destinatários não favorecidos somente se amount < 5000
   if(!to.favored && amount >= 5000) throw {
     status: 400, message: 'Transferências para destinatários não favorecidos devem ser menores que R$ 5.000,00'
-};
+  };
 
   if(from.balance < amount) throw {
     status: 400, message: 'Saldo insuficiente'
-};
+  };
 
   from.balance -= amount;
   to.balance += amount;
@@ -37,7 +40,7 @@ function transfer({ fromId, toId, amount }){
     toId: to.id,
     amount,
     createdAt: new Date().toISOString()
-};
+  };
 
   transferModel.create(transfer);
 
