81b7d09ecb015e37288e86cdeec3f55496adba11
leuthier <leuthier@users.noreply.github.com>
2025-08-24 20:01:30 -0300

    Removed old file

--- codigos/API-minha/test/sum.test.js
+++
@@ -1,8 +0,0 @@
-const { expect } = require('chai');
-const { sum } = require('../src/utils/sum');
-
-describe('sum', () => {
-  it('deve somar dois números', () => {
-    expect(sum(2,3)).to.equal(5);
-  });
-});
