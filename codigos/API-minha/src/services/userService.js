cda7b18251804760fee7d28ecfdb3d3e87dfe2a6
leuthier <leuthier@users.noreply.github.com>
2025-08-24 20:16:13 -0300

    feat: JWT token returned on login sucsess

--- codigos/API-minha/src/services/userService.js
+++ codigos/API-minha/src/services/userService.js
@@ -1,43 +1,61 @@
-const userModel = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const users = require('../models/userModel');
 const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcryptjs');
+
+function userExists(email) {
+  return !!users.findByEmail(email);
+}
 
 function registerUser({ name, email, password, favored = false, balance = 0 }){
-  if(!name || !email || !password) throw {
-    status: 400, message: 'nome, email e senha são obrigatórios'
-  };
+  if(!name || !email || !password) throw new Error ('Nome, email e senha são obrigatórios');
 
-  const exists = userModel.findByEmail(email);
-  if(exists) throw {
-    status: 409, message: 'usuário já existe'
-  };
+  const exists = userExists(email);
+  if(exists) throw new Error('Usuário já existe');
+
+  const hashedPassword = bcrypt.hashSync(password, 8);
 
   const user = {
     id: uuidv4(),
     name,
     email,
-    password,
+    hashedPassword,
     favored: !!favored,
     balance
   };
 
-  return userModel.create(user);
+  return users.create(user);
 
 }
 
 function listUsers(){
-  return userModel.getAll().map(u => ({ id: u.id, name: u.name, email: u.email, favored: u.favored, balance: u.balance }));
+  return users.getAll().map(u => ({ id: u.id, name: u.name, email: u.email, favored: u.favored, balance: u.balance }));
 }
 
 function authenticate({ email, password }){
-  if(!email || !password) throw { status: 400, message: 'email e senha são obrigatórios' };
-  const user = userModel.findByEmail(email);
-  if(!user || user.password !== password) throw { status: 401, message: 'credenciais inválidas' };
-  // token simples para demonstração
-  return { token: `token-${user.id}`, user: { id: user.id, email: user.email, name: user.name } };
+  if(!email || !password) throw new Error ('Email e senha são obrigatórios');
+  const user = users.findByEmail(email);
+  if(!user || !bcrypt.compareSync(password, user.hashedPassword)) throw new Error ('Credenciais inválidas');
+
+  // Gerar JWT
+  const secret = process.env.JWT_SECRET || 'top-secret';
+  const token = jwt.sign(
+    { id: user.id, email: user.email, name: user.name },
+    secret,
+    { expiresIn: '1h' }
+  );
+
+  return { token, user: { id: user.id, email: user.email, name: user.name, favored: user.favored, balance: user.balance }};
 }
 
 function transfer(payload){
   return transferService.transfer(payload);
 }
 
-module.exports = { registerUser, listUsers, authenticate, transfer };
+module.exports = {
+  registerUser,
+  listUsers,
+  authenticate,
+  transfer,
+  userExists
+};
