bff42b1d7a830776a4e375ad4d34146cd359b779
leuthier <leuthier@users.noreply.github.com>
2025-08-24 20:10:54 -0300

    Updating default accounts ammount and removing a function to clear the users that was never used

--- codigos/API-minha/src/models/transferModel.js
+++ codigos/API-minha/src/models/transferModel.js
@@ -15,5 +15,4 @@ module.exports = {
   // retorna transferências onde o usuário é remetente ou destinatário
   findByUserId: (userId) => transfers.filter(t => t.fromId === userId || t.toId === userId),
   create: (transfer) => { transfers.push(transfer); return transfer; },
-  clear: () => { transfers.length = 0; }
 };
--- codigos/API-minha/src/models/userModel.js
+++ codigos/API-minha/src/models/userModel.js
@@ -1,21 +1,18 @@
 // Banco de dados em memória
 const users = [
   {
-    "email": "victor@leuthier.com"
-  },
-  {
     "id": "eb2d3111-4f9b-4bbe-88ec-89c623c49a46",
     "name": "andre",
     "email": "andre@gmail.com",
     "favored": false,
-    "balance": 10
+    "balance": 5000
   },
   {
     "id": "84e47cef-4783-4398-9d72-6eed9d67bf8e",
     "name": "victor2",
     "email": "victor@leuth.com",
     "favored": true,
-    "balance": 100
+    "balance": 10000
   }
 ]
 
@@ -23,6 +20,5 @@ module.exports = {
   getAll: () => users,
   findByEmail: (email) => users.find(u => u.email === email),
   findById: (id) => users.find(u => u.id === id),
-  create: (user) => { users.push(user); return user; },
-  clear: () => { users.length = 0; }
+  create: (user) => { users.push(user); return user; }
 };
