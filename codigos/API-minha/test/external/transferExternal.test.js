52ad394ecb70db5bf61d3383932c340471db218b
leuthier <leuthier@users.noreply.github.com>
2025-08-26 19:25:19 -0300

    fix: Removing sinon on external test

--- codigos/API-minha/test/external/transferExternal.test.js
+++ codigos/API-minha/test/external/transferExternal.test.js
@@ -1,13 +1,9 @@
 // Bibliotecas
 const request = require('supertest');
-const sinon = require('sinon');
 const { expect } = require('chai');
 
 // Testes
 describe('Transfer Controller - External', () => {
-    afterEach(() => {
-        sinon.restore();
-    });
     describe('POST /transfers', () => {
         it('Quando informo remetente e destinatario inexistentes recebo 400', async () => {
             const resposta = await request('http://localhost:3000')
