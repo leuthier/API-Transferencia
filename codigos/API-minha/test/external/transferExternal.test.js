00e9482af1414c6e10fb26c9e2847a9ae84d8f96
leuthier <leuthier@users.noreply.github.com>
2025-08-26 20:30:44 -0300

    feat: New test added
    Signed-off-by: leuthier <leuthier@users.noreply.github.com>

--- codigos/API-minha/test/external/transferExternal.test.js
+++ codigos/API-minha/test/external/transferExternal.test.js
@@ -1,9 +1,7 @@
-// Bibliotecas
 const request = require('supertest');
 const { expect } = require('chai');
 const API = 'http://localhost:3000';
 
-// Testes
 describe('Transfer - External', () => {
     describe('POST /transfers', () => {
         it('Quando informo remetente e destinatario inexistentes recebo 400', async () => {
@@ -17,10 +15,8 @@ describe('Transfer - External', () => {
             expect(resposta.status).to.equal(400);
             expect(resposta.body.error).to.equal('Usuário remetente ou destinatário não encontrado');
         });
-    });
-
-    describe('POST /transfers', () => {
-        it('Quando informo mesmo id remetente e destinatario recebo ', async () => {
+    
+        it('Quando informo mesmo id remetente e destinatario recebo 400', async () => {
             const resposta = await request(API)
                 .post('/transfers')
                 .send({
@@ -31,5 +27,35 @@ describe('Transfer - External', () => {
             expect(resposta.status).to.equal(400);
             expect(resposta.body.error).to.equal('Não é possível transferir para si mesmo');
         });
+
+        it('Quando informo usuários existentes e valor positivo eu tenho sucesso com 201', async () => {
+            const resposta = await request(API)
+                .post('/transfers')
+                .send({
+                    fromId: "eb2d3111-4f9b-4bbe-88ec-89c623c49a46",
+                    toId: "84e47cef-4783-4398-9d72-6eed9d67bf8e",
+                    amount: 100
+                });
+            expect(resposta.status).to.equal(201);
+            expect(resposta.body).to.have.property('from');
+            expect(resposta.body.from).to.have.property('id', 'eb2d3111-4f9b-4bbe-88ec-89c623c49a46');
+            expect(resposta.body.to).to.have.property('id', '84e47cef-4783-4398-9d72-6eed9d67bf8e');
+            expect(resposta.body).to.have.property('transfer');
+            expect(resposta.body.transfer).to.have.property('id');
+            expect(resposta.body.transfer).to.have.property('amount', 100);
+        });
+
+        it('Quando informo valor negativo para transferir recebo 400', async () => {
+            const resposta = await request(API)
+                .post('/transfers')
+                .send({
+                    fromId: "eb2d3111-4f9b-4bbe-88ec-89c623c49a46",
+                    toId: "84e47cef-4783-4398-9d72-6eed9d67bf8e",
+                    amount: -10
+                });
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body.error).to.equal('O valor deve ser maior que zero');
+        });
+
     });
 });
