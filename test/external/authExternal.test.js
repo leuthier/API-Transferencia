39af6578fd23dcdc3c147ed70d434e7d7858482f
leuthier <leuthier@users.noreply.github.com>
2025-08-28 02:07:03 -0300

    Minor changes on authExternal tests

--- test/external/authExternal.test.js
+++ test/external/authExternal.test.js
@@ -2,8 +2,9 @@ const request = require('supertest');
 const { expect } = require('chai');
 const API = 'http://localhost:3000';
 
+
 describe('Auth - External', () => {
-    describe('POST /auth/', () => {
+    describe('POST /auth/login', () => {
         it('Quando não informo email e senha recebo 400', async () => {
             const resposta = await request(API)
                 .post('/auth/login')
@@ -46,7 +47,12 @@ describe('Auth - External', () => {
                 });
             expect(resposta.status).to.equal(200);
             expect(resposta.body).to.have.property('token');
-            expect(resposta.body.token).to.be.a('string');
+            expect(resposta.body).to.have.property('user');
+            expect(resposta.body.user).to.have.property('id');
+            expect(resposta.body.user.email).to.equal('string');
+            expect(resposta.body.user.name).to.equal('string');
+            expect(resposta.body.user.favored).to.equal(true);
+            expect(resposta.body.user.balance).to.equal(0);
         });
     });
 });
