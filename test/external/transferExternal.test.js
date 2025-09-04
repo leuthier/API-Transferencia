6921f2feaec4b5d5f7616a9592b4543bb8dbfe86
leuthier <leuthier@users.noreply.github.com>
2025-09-04 02:17:53 -0300

    fix: External tests working with authentication

--- test/external/transferExternal.test.js
+++ test/external/transferExternal.test.js
@@ -1,28 +1,30 @@
 const request = require('supertest');
 const { expect } = require('chai');
+const { validate: isUuid } = require('uuid');
 const API = 'http://localhost:3000';
+let token;
 
+beforeEach(async () => {
+     // 1 - Capturar token
+    const res = await request(API)
+        .post('/auth/login')
+        .send({
+            email: "string",
+            password: "string"
+        });
+    token = res.body.token;
+});
 
 describe('Transfer - External', () => {
     describe('POST /transfers', () => {
         it('Quando informo destinatario inexistente recebo 400', async () => {
-            // 1 - Capturar token
-            const respostaLogin = await request(API)
-                .post('/auth/login')
-                .send({
-                    email: "string",
-                    password: "string"
-                });
-
-            const token = respostaLogin.body.token;
-
             // 2 - Usar token na requisição de transferência
             const resposta = await request(API)
                 .post('/transfers')
                 .set('Authorization', `Bearer ${token}`)
                 .send({
                     fromEmail: "string",
-                    toEmail: "victor",
+                    toEmail: "victor@gmail.com",
                     amount: 100
                 });
             expect(resposta.status).to.equal(400);
@@ -32,9 +34,10 @@ describe('Transfer - External', () => {
         it('Quando informo mesmo id remetente e destinatario recebo 400', async () => {
             const resposta = await request(API)
                 .post('/transfers')
+                .set('Authorization', `Bearer ${token}`)
                 .send({
-                    fromId: "eb2d3111-4f9b-4bbe-88ec-89c623c49a46",
-                    toId: "eb2d3111-4f9b-4bbe-88ec-89c623c49a46",
+                    fromEmail: "string",
+                    toEmail: "string",
                     amount: 100
                 });
             expect(resposta.status).to.equal(400);
@@ -44,31 +47,50 @@ describe('Transfer - External', () => {
         it('Quando informo usuários existentes e valor positivo eu tenho sucesso com 201', async () => {
             const resposta = await request(API)
                 .post('/transfers')
+                .set('Authorization', `Bearer ${token}`)
                 .send({
-                    fromId: "eb2d3111-4f9b-4bbe-88ec-89c623c49a46",
-                    toId: "84e47cef-4783-4398-9d72-6eed9d67bf8e",
+                    fromEmail: "string",
+                    toEmail: "victor@leuth.com",
                     amount: 100
                 });
+            const createdAt = resposta.body.transfer.createdAt;
+            const dateObject = new Date(createdAt);
             expect(resposta.status).to.equal(201);
-            expect(resposta.body).to.have.property('from');
-            expect(resposta.body.from).to.have.property('id', 'eb2d3111-4f9b-4bbe-88ec-89c623c49a46');
-            expect(resposta.body.to).to.have.property('id', '84e47cef-4783-4398-9d72-6eed9d67bf8e');
-            expect(resposta.body).to.have.property('transfer');
-            expect(resposta.body.transfer).to.have.property('id');
-            expect(resposta.body.transfer).to.have.property('amount', 100);
+            expect(isUuid(resposta.body.transfer.id)).to.be.true;
+            expect(resposta.body.transfer).to.have.property( 'amount', 100);
             expect(resposta.body.transfer).to.have.property('createdAt');
+            // Verifica se createdAt é uma data ISO 8601 válida
+            expect(dateObject.toISOString()).to.equal(createdAt);
+            expect(resposta.body.transfer.from).to.have.property('id', 'b81a5ae0-32fc-43b0-8f33-dbe419cd6033');
+            expect(resposta.body.transfer.from).to.have.property('email', 'string');
+            expect(resposta.body.transfer.to).to.have.property('id', '84e47cef-4783-4398-9d72-6eed9d67bf8e');
+            expect(resposta.body.transfer.to).to.have.property('email', 'victor@leuth.com');
         });
 
         it('Quando informo valor negativo para transferir recebo 400', async () => {
             const resposta = await request(API)
                 .post('/transfers')
+                .set('Authorization', `Bearer ${token}`)
                 .send({
-                    fromId: "eb2d3111-4f9b-4bbe-88ec-89c623c49a46",
-                    toId: "84e47cef-4783-4398-9d72-6eed9d67bf8e",
+                    fromEmail: "string",
+                    toEmail: "andre@gmail.com",
                     amount: -10
                 });
             expect(resposta.status).to.equal(400);
             expect(resposta.body.error).to.equal('O valor deve ser maior que zero');
+        });
+
+        it('Quando tento transferir valores de uma conta que não é a minha recebo 403', async () => {
+            const resposta = await request(API)
+                .post('/transfers')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    fromEmail: "victor@leuth.com",
+                    toEmail: "andre@gmail.com",
+                    amount: 10
+                });
+            expect(resposta.status).to.equal(403);
+            expect(resposta.body.error).to.equal('Você só pode transferir valores da sua própria conta');
         });
 
     });
